06a77d6c85d6dbcd670555d532e9da51bfb09a07
정인권 <wjd03215@naver.com>
2021-06-15 23:57:26 +0900

    Day 22, LeetCode Problems[111, 559]

--- LeetCode/src/bfs/559. Maximum Depth of N-ary Tree.kt
+++ LeetCode/src/bfs/559. Maximum Depth of N-ary Tree.kt
@@ -9,7 +9,19 @@ package maximumdepthofnarytree
 
 class Solution {
     fun maxDepth(root: Node?): Int {
+        if(root == null)
+            return 0
 
+        var max = 0
+        root.children.forEach { node ->
+            if(node != null) {
+                val depth = maxDepth(node)
+                if(max < depth)
+                    max = depth
+            }
+        }
+
+        return max + 1
     }
 }
 
