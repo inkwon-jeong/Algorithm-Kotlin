2c43af362025cfe992ea75d097814b260130e57b
정인권 <wjd03215@naver.com>
2021-08-26 21:35:39 +0900

    Day 93, LeetCode Problems[201]

---
+++ LeetCode/src/bitmanipulation/201. Bitwise AND of Numbers Range.kt
@@ -0,0 +1,17 @@
+package bitwiseandofnumbersrange
+
+class Solution {
+    fun rangeBitwiseAnd(left: Int, right: Int): Int {
+        var m = left
+        var n = right
+        var i = 0
+
+        while (m != n) {
+            m = m shr 1
+            n = n shr 1
+            i++
+        }
+        return m shl i
+    }
+}
+
