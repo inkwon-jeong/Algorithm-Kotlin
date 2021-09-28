f6470dfca6f78c2f9819fb7f7c7806d42031b289
정인권 <wjd03215@naver.com>
2021-09-28 14:29:21 +0900

    Day 126, LeetCode Problems[394]

---
+++ LeetCode/src/stack/394. Decode String.kt
@@ -0,0 +1,32 @@
+package decodestring
+
+import java.util.*
+
+class Solution {
+    fun decodeString(s: String): String? {
+        val queue: Deque<Char> = LinkedList()
+        for (c in s.toCharArray()) queue.offer(c)
+        return decodeString(queue)
+    }
+
+    fun decodeString(queue: Deque<Char>): String {
+        val sb = StringBuilder()
+        var num = 0
+        while (!queue.isEmpty()) {
+            val c = queue.poll()
+            if (Character.isDigit(c)) {
+                num = num * 10 + (c - '0')
+            } else if (c == '[') {
+                val sub = decodeString(queue)
+                for (i in 0 until num) sb.append(sub)
+                num = 0
+            } else if (c == ']') {
+                break
+            } else {
+                sb.append(c)
+            }
+        }
+        return sb.toString()
+    }
+}
+
